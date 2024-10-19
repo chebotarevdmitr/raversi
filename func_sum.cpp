cbf7f1fff6c81ea925fc10d18fbf2f7f2440c8a9
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2024-10-20 00:03:44 +0300

    summa func

--- func_sum.cpp
+++ func_sum.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
+int get_value_from_user(){
+   int x = 0;
+   cout << "\tEnter or integer number :" << endl;
+   cin >> x;
+   return x;
+}
+
 int main(){
-   cout << "HELLO" << endl;
+   cout << "HELLO Dimcasic " << endl;
+   int a = get_value_from_user();
+   int b = get_value_from_user();
+
+   cout << "\t The sum of the functions called " << a << " + "<< b << " = " << a + b << endl;
    return 0;
 }
